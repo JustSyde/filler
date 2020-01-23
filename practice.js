8f057a75db2ee7ad1c35119378146af1a4c4223d
JustSyde <60188325+JustSyde@users.noreply.github.com>
2020-01-23 23:24:54 +0100

    Add files via upload

--- practice.js
+++ practice.js
@@ -23,4 +23,35 @@ $(function(){
         $text.show();
         $overlay.css("background-color", "rgba(0, 139, 186, 0.75)");
       });
+
+    var $slides = $('.slides');
+    var $left = $('.leftarr');
+    var $right = $('.rightarr');
+    var $counter = 1;
+    var $imgWidth = 1100;
+    var $numberOfImages = 6;
+
+    $right.on('click', function(){
+        if ($counter==$numberOfImages){
+            $slides.css("margin-left", "0px");
+            $counter = 1;
+        } else {
+            $slides.css("margin-left", "-" + $imgWidth*$counter + "px");
+            $counter++;
+        }
+        
+    });
+
+    $left.on('click', function(){
+
+        if ($counter==1){
+            $slides.css("margin-left", "-" + $imgWidth*($numberOfImages-1) + "px");
+            $counter = $numberOfImages;
+        } else {
+            $slides.css("margin-left", "-" + $imgWidth*($counter-2) + "px");
+            $counter--;
+        }
+        
+    });
+
 });
